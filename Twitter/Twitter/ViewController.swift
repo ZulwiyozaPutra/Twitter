116080d0734a6a0e21900fed0363bcd8dd72e5fa
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-02-26 03:49:33 +0700

    Storyboard deleted

--- Twitter/Twitter/ViewController.swift
+++ Twitter/Twitter/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func configureView() {
+        view.backgroundColor = .white
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
